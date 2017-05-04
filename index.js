8a7f8b3043a6c8790d35f52ab1bf4344296ef758
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2017-05-04 13:22:02 -0700

    Cria estrutura projeto

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var consign = require("consign");
+
+const app = express();
+
+consign()
+	.include("models")
+	.then("libs/middlewares.js")
+	.then("routes")
+	.then("libs/boot.js")
+	.into(app);
