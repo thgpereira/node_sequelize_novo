ebeaebcba55e4e0c5f0d4b16cd3d44e01c6406ff
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2017-05-05 06:44:55 -0700

    Adiciona conexão ao db

--- libs/boot.js
+++ libs/boot.js
@@ -1,5 +1,7 @@
 module.exports = app => {
-  app.listen(app.get("port"), () => {
-    console.log("NTask API - Porta " + app.get("port"));
+  app.db.sync().done(() => {
+    app.listen(app.get("port"), () => {
+      console.log("NTask API - Porta " + app.get("port"));
+    });
   });
 }
